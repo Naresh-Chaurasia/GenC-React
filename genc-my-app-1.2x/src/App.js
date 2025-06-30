620c6ea552fb64f7eba46ba710aea5f70286ea84
Naresh Chaurasia <johndoe@example.com>
2025-06-30 09:06:59 +0530

    Macbook Pro, Checkin Timestamp::Mon Jun 30 09:06:59 IST 2025

---
+++ genc-my-app-1.2x/src/App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodoRowItem1 from "./components/TodoRowItem1";
+import TodoRowItem2 from "./components/TodoRowItem2";
+
+function App() {
+  const todos = [
+    { rowNumber: 1, rowDescription: "Feed Dog", rowAssigned: "NC" },
+    { rowNumber: 2, rowDescription: "Feed Dog2", rowAssigned: "NC2" },
+    { rowNumber: 3, rowDescription: "Feed Dog3", rowAssigned: "NC3" },
+  ];
+
+  return (
+    <div className="mt-5 container">
+      <div className="card">
+        <div className="card-header">TODO 1.2</div>
+        <div className="card-body">
+          <table className="table table-hover">
+            <thead>
+              {/* Each th with scope="col" indicates that it is a header for the column below it. */}
+              <th scope="col">#</th>
+              <th scope="col">Description</th>
+              <th scope="col">Assigned</th>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Feed Dog</td>
+                <td>NC</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Dance</td>
+                <td>NC</td>
+              </tr>
+
+              <TodoRowItem1 />
+
+              <TodoRowItem2
+                rowNumber={todos[0].rowNumber}
+                rowDescription={todos[0].rowDescription}
+                rowAssigned={todos[0].rowAssigned}
+              />
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
