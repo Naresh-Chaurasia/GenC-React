64b7bb41ee13d2eca4475fa28f65b1e0020daf81
Naresh Chaurasia <johndoe@example.com>
2025-11-30 09:15:21 +0530

    Macbook Pro, Checkin Timestamp::Sun Nov 30 09:15:21 IST 2025

--- genc-my-app-1.3x/src/components/TodoRowItem2.js
+++ genc-my-app-1.3x/src/components/TodoRowItem2.js
@@ -1,9 +1,11 @@
+// Functional component that renders a single row in the todo list table
+// Takes three props: rowNumber (position), rowDescription (task), and rowAssigned (assignee)
 function TodoRowItem2(props) {
   return (
     <tr>
-      <th scope="row">{props.rowNumber}</th>
-      <td>{props.rowDescription}</td>
-      <td>{props.rowAssigned}</td>
+      <th scope="row">{props.rowNumber}</th>  {/* Display the todo item number */}
+      <td>{props.rowDescription}</td>         {/* Display the todo description */}
+      <td>{props.rowAssigned}</td>            {/* Display who the task is assigned to */}
     </tr>
   );
 }
