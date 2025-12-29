bbddb997fb007015216f834c3c6fa3b119bc011d
Naresh Chaurasia <johndoe@example.com>
2025-12-29 21:21:32 +0530

    Macbook Pro, Checkin Timestamp::Mon Dec 29 21:21:32 IST 2025

--- fullstack-library-app-2x/03-frontend-3x/react-library/src/App.tsx
+++ fullstack-library-app-2x/03-frontend-3x/react-library/src/App.tsx
@@ -1,19 +1,15 @@
+import React from "react";
+import { UserContext } from "./appcontext/UserContext";
+import Dashboard from "./appcontext/Dashboard";
 
-import UserFormRegister1 from "./reacthookforms/UserFormRegister1";
-import UserFormSubmit2 from "./reacthookforms/UserFormSubmit2";
-import UserFormSubmit3 from "./reacthookforms/UserFormSubmit3";
-import UserFormSubmit4 from "./reacthookforms/UserFormSubmit4";
+function App() {
+    const user = "Naresh";
 
-export const App = () => {
     return (
-        <div style={{ padding: "20px" }}>
-            <h1>React Hook Form Demo</h1>
-
-            {/* Render the form component */}
-            {/* <UserFormRegister1 /> */}
-            {/* <UserFormSubmit2 /> */}
-            {/* <UserFormSubmit3 /> */}
-            <UserFormSubmit4 />
-        </div>
+        <UserContext.Provider value={user}>
+            <Dashboard />
+        </UserContext.Provider>
     );
 }
+
+export default App;
